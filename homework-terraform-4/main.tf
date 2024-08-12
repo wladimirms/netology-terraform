bcb11df4df0453aa59a4ac666555046bc8d51204
Vladimir Muravev <47062204+wladimirms@users.noreply.github.com>
2024-08-12 22:55:06 +0300

    task 3

--- homework-terraform-4/main.tf
+++ homework-terraform-4/main.tf
@@ -5,22 +5,45 @@ module "vpc_dev" {
   cidr = "10.0.1.0/24"
 }
 
-module "vms-hw4" {
+module "vm-marketing" {
   source                 = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  instance_name          = "marketing"
   env_name               = var.env_name
   network_id             = module.vpc_dev.network_id
   subnet_zones           = [var.default_zone]
   subnet_ids             = [module.vpc_dev.subnet_id]
-  instance_count         = 2
+  instance_count         = 1
   image_family           = var.vm_family
   instance_core_fraction = var.core_fraction
   preemptible            = true
   public_ip              = true
-  
-  for_each = [for i in  var.each_owner: i]
 
   labels = { 
-    owner = "${each.value}",
+    owner = "marketing",
+    org = "ooo_roga_and_coputa"
+     }
+
+  metadata = {
+      user-data          = data.template_file.userdata.rendered
+      serial-port-enable = 1
+  }
+}
+
+module "vm-analytics" {
+  source                 = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  instance_name          = "analytics"
+  env_name               = var.env_name
+  network_id             = module.vpc_dev.network_id
+  subnet_zones           = [var.default_zone]
+  subnet_ids             = [module.vpc_dev.subnet_id]
+  instance_count         = 1
+  image_family           = var.vm_family
+  instance_core_fraction = var.core_fraction
+  preemptible            = true
+  public_ip              = true
+
+  labels = { 
+    owner = "analytics",
     org = "ooo_roga_and_coputa"
      }
 
@@ -36,9 +59,4 @@ data template_file "userdata" {
   vars = {
     ssh_pub_key = file("~/.ssh/id_ed25519.pub")
   }
-}
-
-variable "each_owner" {
-  type = set(string)
-  default =[ "marketing", "analytics"] 
 }
