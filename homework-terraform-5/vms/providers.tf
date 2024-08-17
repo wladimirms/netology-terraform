bb18924070463303c4d6a0d213bc0fb6924b4c39
Vladimir Muravev <47062204+wladimirms@users.noreply.github.com>
2024-08-17 14:49:13 +0300

    hw 5

--- homework-terraform-5/vms/providers.tf
+++ homework-terraform-5/vms/providers.tf
@@ -3,6 +3,14 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
+    template = {
+      # Custom provider forks the original provider.
+      # https://registry.terraform.io/providers/gxben/template/latest
+      # Only difference is the darwin arm binary.
+      source  = "gxben/template"
+      # version has to be set explicitly instead of using a > sign
+      version = "= 2.2.0-m1"
+    }
   }
   required_version = "~>1.8.4"
 }
